411e6777a8257652d80ce9a1cffe6d84372ee2b3
Andrew Markham-Davies <atmd1983@gmail.com>
2023-10-17 17:21:34 +0100

    chore: add trace

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
+
 const fastify = Fastify({
   logger: true
-})
+});
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
-})
+});
 
 // Run the server!
 try {
+  console.log(process.env)
   await fastify.listen({ port: process.env.PORT || 3000 })
 } catch (err) {
   fastify.log.error(err)
